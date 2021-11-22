fafdf4dd6a499f9d77833f0e58ce28a7e1ad702e
wayson <wayson.1104@gmail.com>
2021-11-22 21:49:19 +0100

    Sass module added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-// import './App.scss';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      Burrito.
+      burrito
+      {/* <img src="https://media1.tenor.com/images/3a7824e8bbb622d1f72d263e07027771/tenor.gif?itemid=8974053"/> */}
     </div>
   );
 }
