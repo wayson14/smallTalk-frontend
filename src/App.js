634548d9f4c3f35a95096be7039c468b3b0f4572
wayson14 <wayson.1104@gmail.com>
2022-01-17 20:24:11 +0100

    Finally I figured out the true nature of useState and useEffect hook.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import './App.scss';
 import Chat from './Chat.js';
+
+import { useState, useEffect} from 'react'
+import { AppBar, Typography, Toolbar, Button, CssBaseline} from '@mui/material'
+
 function App() {
+
+  const [username, setUsername] = useState('');
+
   return (
+    
     <div className="App">
+      <CssBaseline/>
+      {/* <AppBar position="relative" sx={{
+        backgroundColor: 'white',
+        color: 'black',
+      }}>
+        <Toolbar>
+          <Button variant="contained" onClick={()=>setUsername('admin')}>Login</Button>
+          <Button variant="outlined" onClick={()=>setUsername('')}>Logout</Button>
+          <Typography>{username}</Typography>
+        </Toolbar>
+      </AppBar> */}
       <Chat></Chat>
     </div>
   );
---
+++ src/Chat.js
@@ -0,0 +1,52 @@
+import { Button, TextField, Paper, CardHeader} from '@mui/material';
+import { React, useState, useEffect } from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+
+
+const Chat = () => {
+
+    const [message, setMessage] = useState('');
+    const [name, setName] = useState('default');
+    const [messagesArray, setMessagesArray] = useState([]);
+
+
+    let room = 1;
+
+    const url = `ws://127.0.0.1:8000/ws/chat/${room}/`
+    const client = new W3CWebSocket(url);
+
+    useEffect(() => {
+        client.onopen = () => {
+            console.log('WebSocket Client Connected')
+        }
+
+        client.onmessage = (message) => {
+            const dataFromServer = JSON.parse(message.data);
+            setMessagesArray(messagesArray => [...messagesArray, dataFromServer])
+
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log(messagesArray);
+    },[messagesArray])
+
+    return (
+        <div>
+            <TextField 
+                id="outlined-basic"
+                variant="outlined"
+                onChange={(e)=>setMessage(e.target.value)}/>
+            <Button variant="outlined" onClick={(e)=>{
+                client.send(JSON.stringify({
+                    type: "message",
+                    message: message,
+                    username: name
+                }));
+                e.preventDefault();
+            }}>send message</Button>
+        </div>
+    )
+}
+
+export default Chat
